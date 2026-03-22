384bcfd4c30581db99847c8388fed97b61f26073
Mello <filipmello2006@gmail.com>
2026-03-22 15:13:51 -0300

    feat(service): implementação do FornecedorService com operações CRUD

---
+++ lista.exercicios/src/main/java/trabalho/java/lista/exercicios/Fornecedores/service/FornecedorService.java
@@ -0,0 +1,34 @@
+package trabalho.java.lista.exercicios.Fornecedores.service;
+
+import trabalho.java.lista.exercicios.Fornecedores.entity.Fornecedor;
+import trabalho.java.lista.exercicios.Fornecedores.repository.FornecedorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FornecedorService {
+
+    private final FornecedorRepository repository;
+
+    public FornecedorService(FornecedorRepository repository) {
+        this.repository = repository;
+    }
+
+    public Fornecedor salvar(Fornecedor fornecedor) {
+        return repository.save(fornecedor);
+    }
+
+    public List<Fornecedor> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Fornecedor> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
